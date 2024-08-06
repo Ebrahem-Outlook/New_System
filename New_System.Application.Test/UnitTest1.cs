1949edfe72609296cd538da341299c112773420e
EbrahemElbetar1@outlook.com <EbrahemElbetar1@outlook.com>
2024-08-07 00:02:15 +0300

    V_7

---
+++ New_System.Application.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace New_System.Application.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ New_System.Infrastructure.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace New_System.Infrastructure.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
